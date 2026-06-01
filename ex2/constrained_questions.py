4dac96a3be18c413922dc8b75a38afcd1174697d
lior-alafi <lior.alafi1@gmail.com>
2026-06-01 19:26:27 +0300

    constrained

--- ex2/constrained_questions.py
+++ ex2/constrained_questions.py
@@ -12,14 +12,25 @@ messages = [
     {"role": "user", "content": msg},
 ]
 
-questions = []
+questions = [
+"Explain why the moon changes shape during the month.",
+"Explain why rainbows appear after rain.",
+"Explain why we need sleep.",
+"Explain why plants need sunlight.",
+"Explain why people recycle paper and plastic.",
+"Explain why magnets stick to some metals.",
+"Explain why brushing your teeth is important.",
+"Explain why traffic lights are useful.",
+"Explain why washing hands helps prevent illness.",
 
-with open('english_hebrew_questions_200.jsonl', 'r', encoding='utf-8') as f:
-    for line in f:
-        data = json.loads(line)
-        questions.append(data['question'])
-        if  len(questions) == 10:
-            break
+]
+
+# with open('english_hebrew_questions_200.jsonl', 'r', encoding='utf-8') as f:
+#     for line in f:
+#         data = json.loads(line)
+#         questions.append(data['question'])
+#         if  len(questions) == 10:
+#             break
 
 csv_path = 'constrained_questions.csv'
 
@@ -33,7 +44,7 @@ with open(csv_path, 'w', encoding='utf-8-sig', newline='') as f:
 
 model_inf = [{'id': 'mistralai/Mistral-7B-Instruct-v0.3','allowed_token_path': 'hebrew_allowed_tokens_mistral.json','prefix':"ענה בעברית"+"\n\n"},
              {'id': 'Qwen/Qwen2.5-7B-Instruct','allowed_token_path': 'hebrew_allowed_tokens_qwen.json','prefix':'answer only in Hebrew and check your output\n\n'} ]
-
+prefix='answer only in Hebrew and check your output\n\n'
 for m in model_inf:
 
     generator = HFChatGenerator( model_id=m['id'],
@@ -42,7 +53,7 @@ for m in model_inf:
     generator.print_device_info()
     for question in questions:
         messages = [
-            {"role": "user", "content": 'answer only in Hebrew and check your output\n\n'+question},
+            {"role": "user", "content": prefix+question},
         ]
 
         constrained_answer = generator.generate_constrained(
